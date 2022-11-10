09f4df858f09517f0207408239d33aa2d822d7e1
hassan <hassan.elmrabett@gmail.com>
2022-11-10 21:52:32 +0100

    Create Class

---
+++ Models/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes.Models
+{
+    public class Shape
+    {
+        public string ID { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public int AmountSides { get; set; }
+        public float[] Position { get; set; }
+        public float Scale { get; set; }
+        public float[] Rotation { get; set; }
+
+    }
+}
